1894c79115be5ad965e8c3cff54777f55af0ddfd
Bala Maniknata <balamanikanta.m@inncircles.com>
2026-03-17 11:17:35 +0530

    chore: update axios baseURL to live render production server

--- frontend/src/store/useStore.js
+++ frontend/src/store/useStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://127.0.0.1:5000/api' });
+const api = axios.create({ baseURL: 'https://jira-clone-server.onrender.com/api' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
